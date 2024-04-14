c941e5901c148de96c2a34f0062c36b7d24028f3
Otacilio <otacilio.engmecanica@gmail.com>
2024-04-13 21:26:47 -0300

    commit teste do deploy

--- manutencao/urls.py
+++ manutencao/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
-<<<<<<< HEAD
-path('home_manutencao/', views.home_manutencao, name='home_manutencao')
-=======
-path('homemanutencao/', views.home_manutencao, name='home_manutencao')
->>>>>>> 8effb2bc7dabb31835438e2bacc7d1b9c3b6302f
+
+path('homemanutencao/', views.home_manutencao, name='homemanutencao')
+
+
 ]
