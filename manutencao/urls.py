f9ac488725da1d4de04c55dc026984fa1b505770
Otacílio <otacilio.engmecanica@gmail.com>
2024-04-15 16:58:06 -0300

    manutencao 2

--- manutencao/urls.py
+++ manutencao/urls.py
@@ -4,12 +4,5 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
 path('home_manutencao/', views.home_manutencao, name='home_manutencao')
-=======
-
-path('homemanutencao/', views.home_manutencao, name='homemanutencao')
-
-
->>>>>>> c941e5901c148de96c2a34f0062c36b7d24028f3
 ]
