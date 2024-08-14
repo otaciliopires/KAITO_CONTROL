5c97f99f690973f44abf8112a87563d00d6fe7c0
Otacilio <otacilio.engmecanica@gmail.com>
2024-08-14 14:09:16 -0300

    configurações https

--- kaito_fuel/settings.py
+++ kaito_fuel/settings.py
@@ -153,3 +153,11 @@ MESSAGE_TAG = {
     constants.INFO: "alert alert-info",
     constants.WARNING: "alert alert-warning"
 }
+
+#HTTPS/SSL
+
+if not DEBUG:
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+	SECURE_SSL_REDIRECT = True
+	SESSION_COOKIE_SECURE = True
+	CSRF_COOKIE_SECURE = True
