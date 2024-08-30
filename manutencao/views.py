a5c2fe9e73db98c12a72a3d767820447c0726c70
Otacilio <otacilio.engmecanica@gmail.com>
2024-08-29 23:12:57 -0300

    adição de tempos em serviços realizada

--- manutencao/views.py
+++ manutencao/views.py
@@ -144,7 +144,6 @@ def servico_oficina(request, id):
             data_fim = request.POST.get('data_fim')
             data_status = request.POST.get('data_status')
             att_tempo_1_os(id, data_status)
-            att_tempo_1_servico(id_servico,data_status)
             status_servico = request.POST.get('status_servico')
             executante_funcionario_id = request.POST.get('executante_funcionario')
             if executante_funcionario_id == None:
@@ -172,10 +171,30 @@ def servico_oficina(request, id):
                 servico_oficina.data_mudanca_status = data_fim
                 data_status = data_fim
 
+
+            data_status = datetime.strptime(data_status, "%Y-%m-%dT%H:%M")
+            if servico_oficina.status == "Em Serviço":
+                    print("testserv",servico_oficina.status, (data_status.timestamp() - servico_oficina.data_mudanca_status.timestamp())/3600)
+                    servico_oficina.tempo_em_servico = servico_oficina.tempo_em_servico + (data_status.timestamp() - servico_oficina.data_mudanca_status.timestamp())/3600
+                    servico_oficina.data_mudanca_status = data_status
+                    servico_oficina.status = status_servico
+            elif servico_oficina.status == "Aguardando Peças":
+                    print("testeap",servico_oficina.status, (now.timestamp() - servico_oficina.data_mudanca_status.timestamp())/3600)
+                    servico_oficina.tempo_aguardo_peca= servico_oficina.tempo_aguardo_peca + (data_status.timestamp() - servico_oficina.data_mudanca_status.timestamp())/3600
+                    servico_oficina.data_mudanca_status = data_status
+                    servico_oficina.status = status_servico
+
+            elif servico_oficina.status == 'Aguardando Serviço':
+                    print("testeas",servico_oficina.status, (now.timestamp() - servico_oficina.data_mudanca_status.timestamp())/3600)
+                    servico_oficina.tempo_aguardo_servico = servico_oficina.tempo_aguardo_servico + (data_status.timestamp() - servico_oficina.data_mudanca_status.timestamp())/3600
+                    servico_oficina.data_mudanca_status = data_status
+                    servico_oficina.status = status_servico
+
             servico_oficina.save()
-                #colocar a função antes de salvar as informações no BD garante que o valor calculado de tempo seja contabilizado para o status anterior(correto)  
-               
-    
+            print(Servico_Oficina.objects.get(id=id_servico).status)
+                        #colocar a função antes de salvar as informações no BD garante que o valor calculado de tempo seja contabilizado para o status anterior(correto)  
+                
+        
               
 
         
